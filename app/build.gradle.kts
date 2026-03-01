550a47d3db40563dea89a6a64893c91fa97d30ba
Shreeya <shreeya@supradigit.com>
2026-03-01 21:06:49 +0530

    Config Setup3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.tasha.pdbuilder"
         minSdk = 29
         targetSdk = 36
-        versionCode = 101
-        versionName = "1.0.1"
+        versionCode = 102
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
